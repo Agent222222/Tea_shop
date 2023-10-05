e37b7619853e7b5fb4188a9ab2ec188b5a388f14
kol1a_ <koljanmuzyka2108@gmail.com>
2023-10-05 15:23:10 +0300

    almost done main page

--- Frontend/src/js/app.js
+++ Frontend/src/js/app.js
@@ -1,6 +1,19 @@
-import * as flsFunctions from "./modules/functions.js";
+import * as dropFunctions from "./modules/functions.js";
 
+const cross = document.querySelectorAll('section6-part2-question-img');
+// cross.addEventListener("click", (event) => {
+//     cross.style.border = "1px solid $color_1";
+//     cross.style.transition = "0.5s";
+//     cross.style.rotate = "135deg"; 
+// });
+  
+console.log("dkiufgbhisd");
 
+let btns = document.querySelectorAll('button-subscription');
 
-
+btns.forEach(btn => {
+    btn.addEventListener("click", (event) => {
+        console.log("dkiu5555isd");
+    });
+});
 
--- Frontend/src/js/modules/functions.js
+++ Frontend/src/js/modules/functions.js
@@ -1,14 +1,3 @@
-export function isWebp () {
-    function testWebp(callback) {
-        let webP = new Image();
-        webP.onload = webP.onerror = function () {
-            callback(webP.height == 2);
-        };
-        webP.src = "/src/img/IMG_20200703_143610_copy.webp"
-    }
-
-    testWebp(function (support){
-        let className = support === true ? 'webp' : 'no-webp';
-        document.documentElement.classList.add(className);
-    });
+export function faq () {
+    
 }
