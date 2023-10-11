0579653d89ab28f6a0fc3915f910cfe98640c81c
kol1a_ <koljanmuzyka2108@gmail.com>
2023-10-11 21:07:44 +0300

    finished tablet version of main page

--- Frontend/src/js/app.js
+++ Frontend/src/js/app.js
@@ -3,22 +3,28 @@ const questions = document.querySelectorAll('.section6-part2-question');
 function resetAllElements() {
     questions.forEach((question) => {
         const image = question.querySelector('.section6-part2-question-img');
+        var tabWidth = document.documentElement.clientWidth;
         const answer = question.querySelector('.hidden');
         const parent = image.parentElement;
-
+        console.log(tabWidth);
         image.style.border = "0px";
         image.style.transform = "rotate(0deg)";
         answer.style.height = '0';
-        parent.style.height = 'auto';
+        if(tabWidth < 1500){
+            parent.style.height = '60px';
+        }else{
+            parent.style.height = '100px';
+        }
     });
 }
 
 questions.forEach((question) => {
-    let clickCount = 0;
     const image = question.querySelector('.section6-part2-question-img');
+    var tabWidth = document.documentElement.clientWidth;
     const answer = question.querySelector('.hidden');
     const parent = image.parentElement;
-
+    let clickCount = 0;
+    console.log(tabWidth);
     image.addEventListener("click", (event) => {
         resetAllElements();
 
@@ -27,10 +33,15 @@ questions.forEach((question) => {
             event.target.style.transition = "0.5s";
             event.target.style.transform = "rotate(135deg)";
             answer.style.height = '30px';
-            parent.style.height = '130px';
+            if(tabWidth < 1500){
+                parent.style.height = '90px';
+            }else{
+                parent.style.height = '130px';
+            }
             clickCount++;
         } else {
             clickCount = 0;
         }
     });
 });
+
